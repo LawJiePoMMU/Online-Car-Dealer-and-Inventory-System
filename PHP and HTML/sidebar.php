0c60c80ecf5f3452114988d8dd7b57fb3a9b8572
LawJiePoMMU <LAW.JIE.PO@student.mmu.edu.my>
2026-03-16 20:16:02 +0800

    git add PHP and HTML/dashboard.php git add PHP and HTML/sidevar.php git add CSS/admin.css git modified PHP and HTML/database.php

---
+++ PHP and HTML/sidebar.php
@@ -0,0 +1,60 @@
+<aside class="sidebar">
+    <div class="sidebar-brand">
+        <i class="fas fa-circle-notch logo-icon"></i>
+        <h2>Admin</h2>
+    </div>
+
+    <div class="sidebar-section">NAVIGATION</div>
+
+    <ul class="sidebar-menu">
+        <li>
+            <a href="dashboard.php">
+                <i class="fas fa-th-large"></i> <span>Dashboard</span>
+            </a>
+        </li>
+        <li>
+            <a href="manage-cars.php">
+                <i class="fas fa-car"></i> <span>Manage Cars</span>
+            </a>
+        </li>
+        <li>
+            <a href="specifications.php">
+                <i class="fas fa-cogs"></i> <span>Specifications</span>
+            </a>
+        </li>
+        <li>
+            <a href="reservations.php">
+                <i class="fas fa-calendar-alt"></i> <span>Reservations</span>
+            </a>
+        </li>
+        <li>
+            <a href="inventory.php">
+                <i class="fas fa-boxes"></i> <span>Inventory</span>
+            </a>
+        </li>
+        <li>
+            <a href="users.php">
+                <i class="far fa-users"></i> <span>Users</span>
+            </a>
+        </li>
+        <li>
+            <a href="chat.php">
+                <i class="fas fa-comments"></i> <span>Chat</span>
+            </a>
+        </li>
+    </ul>
+
+    <div class="sidebar-section">GENERAL</div>
+    <ul class="sidebar-menu">
+        <li>
+            <a href="settings.php">
+                <i class="fas fa-cog"></i> <span>Settings</span>
+            </a>
+        </li>
+    </ul>
+    <div class="sidebar-footer"></div>
+    <a href="logout.php">
+        <i class="fas fa-sign-out-alt"></i> <span>Log Out</span>
+    </a>
+    </div>
+</aside>
