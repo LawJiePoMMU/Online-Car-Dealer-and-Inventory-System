3baeaaf0d88d410fbde44d0286a94f800caba1c6
LawJiePoMMU <LAW.JIE.PO@student.mmu.edu.my>
2026-03-18 13:22:44 +0800

    Move HTML files reservation, reservation confirm, payment, and payment confirm to Users folder and add logout.php

---
+++ PHP and HTML/logout.php
@@ -0,0 +1,9 @@
+<?php
+session_start();
+$_SESSION = array();
+session_destroy();
+echo "<script>
+            alert('You have been logged out.');
+            window.location.href = 'index.php';
+     </script>";
+?>
