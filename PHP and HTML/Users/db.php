abc72140763301d0c0254fc1afc1a3cf3e70b686
WONG YEW JONN <your_email@example.com>
2026-05-17 00:55:44 +0800

    Updated reservation booking and payment system

--- PHP and HTML/Users/db.php
+++ PHP and HTML/Users/db.php
@@ -1,13 +1,36 @@
 <?php
+
 $host     = "localhost";
 $dbname   = "online_car_dealer_and_inventory_db";
 $username = "root";
 $password = "";
 
 try {
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    $pdo = new PDO(
+        "mysql:host=$host;dbname=$dbname;charset=utf8mb4",
+        $username,
+        $password
+    );
+
+    // Enable PDO Exceptions
+    $pdo->setAttribute(
+        PDO::ATTR_ERRMODE,
+        PDO::ERRMODE_EXCEPTION
+    );
+
+    // Default Fetch Mode
+    $pdo->setAttribute(
+        PDO::ATTR_DEFAULT_FETCH_MODE,
+        PDO::FETCH_ASSOC
+    );
+
 } catch (PDOException $e) {
-    die("Database connection failed: " . $e->getMessage());
+
+    die(
+        "Database connection failed: " .
+        $e->getMessage()
+    );
 }
+
 ?>
