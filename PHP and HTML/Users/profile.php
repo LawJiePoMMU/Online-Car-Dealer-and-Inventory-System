b7bc80397f50a64fd9bf8ee31daa9107847a5341
cheong06 <mihpp71@gmail.com>
2026-06-07 22:09:58 +0800

    change

--- PHP and HTML/Users/profile.php
+++ PHP and HTML/Users/profile.php
@@ -102,10 +102,30 @@ include 'Includes/header.php';
 
                 <!-- 🔥 替换为高级感的极简 SVG 图标 -->
                 <ul class="profile-menu">
-                    <li><a href="profile.php" class="active">👤 My Profile</a></li>
-                    <li><a href="wishlist.php">❤️ My Wishlist</a></li>
-                    <li><a href="view_status.php">📅 My Satus</a></li> 
-                    <li><a href="Auth/logout.php" style="color: #dc2626;">🚪 Logout</a></li>
+                    <li>
+                        <a href="profile.php" class="active">
+                            <svg viewBox="0 0 24 24"><path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path><circle cx="12" cy="7" r="4"></circle></svg>
+                            My Profile
+                        </a>
+                    </li>
+                    <li>
+                        <a href="wishlist.php">
+                            <svg viewBox="0 0 24 24"><path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"></path></svg>
+                            My Wishlist
+                        </a>
+                    </li>
+                    <li>
+                        <a href="view_status.php">
+                            <svg viewBox="0 0 24 24"><rect x="3" y="4" width="18" height="18" rx="2" ry="2"></rect><line x1="16" y1="2" x2="16" y2="6"></line><line x1="8" y1="2" x2="8" y2="6"></line><line x1="3" y1="10" x2="21" y2="10"></line></svg>
+                            My Bookings
+                        </a>
+                    </li>
+                    <li>
+                        <a href="Auth/logout.php" class="logout-link">
+                            <svg viewBox="0 0 24 24"><path d="M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4"></path><polyline points="16 17 21 12 16 7"></polyline><line x1="21" y1="12" x2="9" y2="12"></line></svg>
+                            Logout
+                        </a>
+                    </li>
                 </ul>
             </aside>
 
